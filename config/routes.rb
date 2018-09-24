175cd5c21698638cb99a3588a11459e69f8a45e6
Crystal Ramirez <crystal9563@gmail.com>
2018-09-24 10:31:16 -0400

    user model complete with testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
   get 'static_pages/home'
   get 'static_pages/about'
   get 'static_pages/help'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users
   root 'static_pages#home'
 end
